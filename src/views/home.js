5fe1690104b11137a75e7eb633cbbbb8178f1aae
JoaoBarreto03 <211039519@aluno.unb.br>
2023-01-12 11:06:31 -0300

    Pgs. Home e Sobre (about) finalizadas

--- src/views/home.js
+++ src/views/home.js
@@ -16,6 +16,14 @@ class Home extends React.Component{
                     <img width="800" height="500" src="https://raw.githubusercontent.com/fga-eps-mds/2022-2-UnBoard/front/Imagens/Design%20sem%20nome.png"/>
                 </div>
                <h3>Informações sobre o ingresso na Universidade de Brasília</h3>
+               <br></br>
+               <div className="copy">
+
+                <center>
+                    <h6>Squad6 2023 © </h6>
+                </center>
+
+               </div>
             </div>
 
             </>
