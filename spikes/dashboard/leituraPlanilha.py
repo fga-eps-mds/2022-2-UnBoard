2551550faedd4ca6fd0c3dba5c3c7a3f89178251
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-01-18 12:13:31 -0300

    Update leituraPlanilha.py

--- spikes/dashboard/leituraPlanilha.py
+++ spikes/dashboard/leituraPlanilha.py
@@ -2,8 +2,52 @@ import pandas as pd
 
 df = pd.read_excel("../../pdfs/VESTUNB_23.xlsx")
 
-Nome = "Agronomia (Bacharelado)" #nome que deseja pesquisar
+Nome = "Arquitetura e Urbanismo (Bacharelado)" #nome que deseja pesquisar
 Vazia = "Unnamed: 0" #nome da coluna vazia
+
+#variaveis globais diurno
+global vagas_cotas_negros_diurno
+global inscritos_cotas_negros_diurno
+global vagas_cotas_escolaPublica_negros_def_rendaInf_diurno
+global inscritos_cotas_escolaPublica_negros_def_rendaInf_diurno
+global vagas_cotas_escolaPublica_negros_rendaInf_diurno
+global inscritos_cotas_escolaPublica_negros_rendaInf_diurno
+global vagas_cotas_escolaPublica_negros_def_rendaSup_diurno
+global inscritos_cotas_escolaPublica_negros_def_rendaSup_diurno
+global vagas_cotas_escolaPublica_negros_rendaSup_diurno
+global inscritos_cotas_escolaPublica_negros_rendaSup_diurno
+global vagas_cotas_escolaPublica_def_rendaInf_diurno
+global inscritos_cotas_escolaPublica_def_rendaInf_diurno
+global vagas_cotas_escolaPublica_rendaInf_diurno
+global inscritos_cotas_escolaPublica_rendaInf_diurno
+global vagas_cotas_escolaPublica_def_rendaSup_diurno
+global inscritos_cotas_escolaPublica_def_rendaSup_diurno
+global vagas_cotas_escolaPublica_rendaSup_diurno
+global inscritos_cotas_escolaPublica_rendaSup_diurno
+global vagas_universais_diurno
+global inscritos_universais_diurno
+
+#variaveis globais noturno
+global vagas_cotas_negros_noturno
+global inscritos_cotas_negros_noturno
+global vagas_cotas_escolaPublica_negros_def_rendaInf_noturno
+global inscritos_cotas_escolaPublica_negros_def_rendaInf_noturno
+global vagas_cotas_escolaPublica_negros_rendaInf_noturno
+global inscritos_cotas_escolaPublica_negros_rendaInf_noturno
+global vagas_cotas_escolaPublica_negros_def_rendaSup_noturno
+global inscritos_cotas_escolaPublica_negros_def_rendaSup_noturno
+global vagas_cotas_escolaPublica_negros_rendaSup_noturno
+global inscritos_cotas_escolaPublica_negros_rendaSup_noturno
+global vagas_cotas_escolaPublica_def_rendaInf_noturno
+global inscritos_cotas_escolaPublica_def_rendaInf_noturno
+global vagas_cotas_escolaPublica_rendaInf_noturno
+global inscritos_cotas_escolaPublica_rendaInf_noturno
+global vagas_cotas_escolaPublica_def_rendaSup_noturno
+global inscritos_cotas_escolaPublica_def_rendaSup_noturno
+global vagas_cotas_escolaPublica_rendaSup_noturno
+global inscritos_cotas_escolaPublica_rendaSup_noturno
+global vagas_universais_noturno
+global inscritos_universais_noturno
 
 #filtra apenas os diurnos
 linha_diurno = df.loc[(df[0] == Nome) & (df[Vazia] <= 71)]
@@ -48,11 +92,33 @@ if linha_diurno.shape[0] != 0:
 linha_noturno = df.loc[(df[0] == Nome) & (df[Vazia] > 71)]
 if linha_noturno.shape[0] != 0:
     indice_linha_curso_noturno = linha_noturno.index[0]
+
     vagas_cotas_negros_noturno = df.at[indice_linha_curso_noturno, 1]
     inscritos_cotas_negros_noturno = df.at[indice_linha_curso_noturno, 2]
 
+    vagas_cotas_escolaPublica_negros_def_rendaInf_noturno = df.at[indice_linha_curso_noturno, 4]
+    inscritos_cotas_escolaPublica_negros_def_rendaInf_noturno = df.at[indice_linha_curso_noturno, 5]
+
+    vagas_cotas_escolaPublica_negros_rendaInf_noturno = df.at[indice_linha_curso_noturno, 7]
+    inscritos_cotas_escolaPublica_negros_rendaInf_noturno = df.at[indice_linha_curso_noturno, 8]
+
+    vagas_cotas_escolaPublica_negros_def_rendaSup_noturno = df.at[indice_linha_curso_noturno, 10]
+    inscritos_cotas_escolaPublica_negros_def_rendaSup_noturno = df.at[indice_linha_curso_noturno, 11]
     
+    vagas_cotas_escolaPublica_negros_rendaSup_noturno = df.at[indice_linha_curso_noturno, 13]
+    inscritos_cotas_escolaPublica_negros_rendaSup_noturno = df.at[indice_linha_curso_noturno, 14]
 
+    vagas_cotas_escolaPublica_def_rendaInf_noturno = df.at[indice_linha_curso_noturno, 16]
+    inscritos_cotas_escolaPublica_def_rendaInf_noturno = df.at[indice_linha_curso_noturno, 17]
+    
+    vagas_cotas_escolaPublica_rendaInf_noturno = df.at[indice_linha_curso_noturno, 19]
+    inscritos_cotas_escolaPublica_rendaInf_noturno = df.at[indice_linha_curso_noturno, 20]
 
-print(vagas_cotas_negros_diurno)
-print(inscritos_cotas_negros_diurno)
+    vagas_cotas_escolaPublica_def_rendaSup_noturno = df.at[indice_linha_curso_noturno, 22]
+    inscritos_cotas_escolaPublica_def_rendaSup_noturno = df.at[indice_linha_curso_noturno, 23]
+    
+    vagas_cotas_escolaPublica_rendaSup_noturno = df.at[indice_linha_curso_noturno, 25]
+    inscritos_cotas_escolaPublica_rendaSup_noturno = df.at[indice_linha_curso_noturno, 26]
+
+    vagas_universais_noturno = df.at[indice_linha_curso_noturno, 28]
+    inscritos_universais_noturno = df.at[indice_linha_curso_noturno, 29]
