b38778664656b6eb713ceff30caef0a25bc62c89
frmiza <mizzaelsantos1@gmail.com>
2023-01-18 19:19:12 -0300

    atualizando script

--- backend/deactivate.sh
+++ backend/deactivate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 black .
-flake8 . --select BLK --exclude=env --show-source1
+flake8 . --select BLK --exclude=env --show-source
 deactivate
--- scripts/depslinux.sh
+++ scripts/depslinux.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+sudo apt install curl git-core gcc make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libssl-dev
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+
+
+echo export PYENV_ROOT="$HOME/.pyenv" >> ~/.bashrc
+echo export PATH="$PYENV_ROOT/bin:$PATH" >> ~/.bashrc
+echo "\n"
+echo "if command -v pyenv 1>/dev/null 2>&1; then
+  > echo  eval "$(pyenv init -)"
+fi"
 
 curl -sSL https://install.python-poetry.org | python3 -
 
