3646113b1ffb857a8aa2b964e5eef4830acc942c
frmiza <mizzaelsantos1@gmail.com>
2023-01-30 10:17:24 -0300

    criacao da estrutura do webscraping com scrapy

---
+++ backend/parser/vestunt_etl.py
@@ -0,0 +1,59 @@
+import abc
+import typing
+from pathlib import Path
+from parser.base_etl import BaseETL
+
+
+class VestUnbETL(BaseETL):
+    """
+    Estrutura que manipula arquivos referente aos dados
+    """
+
+    URL: str = "https://www.cebraspe.org.br/vestibulares"
+    URL_ENC: str = f"{URL}/encerrado"
+
+    def __init__(self, input: str, output: str, create_path: bool = True, status: bool = True):
+        """
+        Instancia um objeto ETL
+        :param input: string contendo o diretorio dos dados de entrada
+        :param output: string contendo o diretorio dos dados de saida
+        :patam status: flag que indica se deve ser baixado os dados:w
+        :param create_path: flag que indica se um diretorio deve ser criado
+        """
+
+        self._status = status
+        super().__init__(input, output, create_path)
+
+    def links_vestibular(self) -> typing.Dict[str, str]:
+        pass
+
+    @abc.abstractmethod
+    def extract(self) -> None:
+        """
+        Extrai os dados de algum objeto
+        """
+        pass
+
+    @abc.abstractmethod
+    def transform(self) -> None:
+        """
+        Transforma os dados e os adequa para os formatos
+        de saida de interesse
+        """
+        pass
+
+    @abc.abstractmethod
+    def load(self) -> None:
+        """
+        exporta os dados para um csv
+        """
+        for arq, df in self.data_output.items():
+            df.to_parquet(self.path_output / arq, index=False)
+
+    def pipeline(self) -> None:
+        """
+        executa o pipeline completo
+        """
+        self.extract()
+        self.transform()
+        self.load()
