a04d70c10c803b198000fd52f4a19af6d5c12ef5
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-02-02 17:09:44 -0300

    Create buscarCurso_15.py

---
+++ spikes/dashboard/buscarCurso_15.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_excel("../../pdfs/VESTUNB_15.xlsx")
+
+global Nome
+
+Nome = "Letras — Português do Brasil como Segunda Língua (Licenciatura)" #nome que deseja pesquisar
