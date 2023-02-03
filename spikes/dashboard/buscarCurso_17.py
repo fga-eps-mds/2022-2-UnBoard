2cecb575b04735fd0525246204c8009a88f5d8b9
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-02-02 21:49:24 -0300

    Create buscarCurso_17.py

---
+++ spikes/dashboard/buscarCurso_17.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel("../../pdfs/VESTUNB_17.xlsx")
+
+global Nome
+global Vazia
+
+Nome = "Administração (Bacharelado)" #nome que deseja pesquisar
+Vazia = "Unnamed: 0" #nome da coluna vazia
