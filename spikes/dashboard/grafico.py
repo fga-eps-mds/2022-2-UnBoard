773b91435dc8b43ea5696159a43337db3047e4a6
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-02-02 21:34:43 -0300

    update planilhas

--- spikes/dashboard/grafico.py
+++ spikes/dashboard/grafico.py
@@ -7,38 +7,40 @@ from buscarCurso import *
 
 app = Dash(__name__)
 
-df = pd.DataFrame({
-    "TIPO": ["Negros", "Negros", 
-    "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
-    "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
-    "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
-    "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
-    "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes deficientes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
-    "Estudantes deficientes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
-    "Estudantes de deficientes escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes de deficientes escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
-    "Universal", "Universal"],
-    "Quantidade": [vagas_cotas_negros_diurno, inscritos_cotas_negros_diurno, 
-    vagas_cotas_escolaPublica_negros_def_rendaInf_diurno, inscritos_cotas_escolaPublica_negros_def_rendaInf_diurno,
-    vagas_cotas_escolaPublica_negros_rendaInf_diurno, inscritos_cotas_escolaPublica_negros_rendaInf_diurno
-    , vagas_cotas_escolaPublica_negros_def_rendaSup_diurno, inscritos_cotas_escolaPublica_negros_def_rendaSup_diurno
-    , vagas_cotas_escolaPublica_negros_rendaSup_diurno, inscritos_cotas_escolaPublica_negros_rendaSup_diurno
-    , vagas_cotas_escolaPublica_def_rendaInf_diurno, inscritos_cotas_escolaPublica_def_rendaInf_diurno
-    , vagas_cotas_escolaPublica_rendaInf_diurno, inscritos_cotas_escolaPublica_rendaInf_diurno
-    , vagas_cotas_escolaPublica_def_rendaSup_diurno, inscritos_cotas_escolaPublica_def_rendaSup_diurno
-    , vagas_cotas_escolaPublica_rendaSup_diurno, inscritos_cotas_escolaPublica_rendaSup_diurno, vagas_universais_diurno, 
-    inscritos_universais_diurno],
-    "Legenda": ["Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", 
-    "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos"]
-})
+
+if cont == 1:
+    df = pd.DataFrame({
+        "TIPO": ["Negros", "Negros", 
+        "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
+        "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
+        "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo", 
+        "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
+        "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes deficientes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes deficientes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
+        "Estudantes deficientes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda inferior ou igual a 1,5 salario minimo",
+        "Estudantes de deficientes escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes de deficientes escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Estudantes de escolas publicas <br> nao autodeclarados pretos, pardos ou indígenas <br> com renda superior a 1,5 salario minimo",
+        "Universal", "Universal"],
+        "Quantidade": [vagas_cotas_negros_diurno, inscritos_cotas_negros_diurno, 
+        vagas_cotas_escolaPublica_negros_def_rendaInf_diurno, inscritos_cotas_escolaPublica_negros_def_rendaInf_diurno,
+        vagas_cotas_escolaPublica_negros_rendaInf_diurno, inscritos_cotas_escolaPublica_negros_rendaInf_diurno
+        , vagas_cotas_escolaPublica_negros_def_rendaSup_diurno, inscritos_cotas_escolaPublica_negros_def_rendaSup_diurno
+        , vagas_cotas_escolaPublica_negros_rendaSup_diurno, inscritos_cotas_escolaPublica_negros_rendaSup_diurno
+        , vagas_cotas_escolaPublica_def_rendaInf_diurno, inscritos_cotas_escolaPublica_def_rendaInf_diurno
+        , vagas_cotas_escolaPublica_rendaInf_diurno, inscritos_cotas_escolaPublica_rendaInf_diurno
+        , vagas_cotas_escolaPublica_def_rendaSup_diurno, inscritos_cotas_escolaPublica_def_rendaSup_diurno
+        , vagas_cotas_escolaPublica_rendaSup_diurno, inscritos_cotas_escolaPublica_rendaSup_diurno, vagas_universais_diurno, 
+        inscritos_universais_diurno],
+        "Legenda": ["Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", 
+        "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos", "Vagas", "Inscritos"]
+    })
 
 fig = px.bar(df, x="TIPO", y="Quantidade", color="Legenda", barmode="group")
 #fig.update_layout(yaxis_type='linear')
