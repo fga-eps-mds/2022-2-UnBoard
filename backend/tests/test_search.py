299f8236131bc09c2e7e71093b23a065c024df8c
frmiza <mizzaelsantos1@gmail.com>
2023-02-04 13:09:53 -0300

    testes baiscos realizados

--- backend/tests/test_search.py
+++ backend/tests/test_search.py
@@ -1,34 +1,65 @@
+from _pytest.mark import pytest_addoption
 import pytest
-from app.search_bar import *
+from utils.search_bar import Courses_list
 
-class Test_Search:
-    def setup_method(self):
-        self.search_bar = Courses_list("../../databases")
-    
-    def test_len(self):
-        self.setup_method()
-        result = self.search_bar.search_course("")
-        courses = self.search_bar.ordered_courses
-        assert (len(courses) == 101) and (len(courses) == len(result))
+def test_se_tamanho_de_cursos_eh_0():
+    search_bar = Courses_list(2019)
+    tamanho_esperado = 0
+    resultado = len(search_bar.search_course(""))
+    assert resultado == tamanho_esperado
 
-    def finder(self, exp: str):
-        result = self.search_bar.search_course(exp)
-        for course in result:
-            assert (str(course[0]).find(exp)) != -1
+@pytest.mark.parametrize(
+    ("parametro", "resultado_esperado"),
+    [(2019, 0), (2022, 0), (2023, 0)]
+)
+def test_se_tamanho_de_cursos_por_ano_eh_0_se_passar_nenhum_curso(parametro, resultado_esperado):
+    search_bar = Courses_list(parametro)
+    resultado = len(search_bar.search_course(""))
+    assert resultado == resultado_esperado
 
-    def test_with_finder(self):
-        self.setup_method()
-        self.finder("Adm")
-        self.finder("")
-        self.finder("Bacharelado")
-        self.finder("Engenharia")
-        self.finder("dnoawkdnsknawod")
+#FALHA ESPERADA
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    ("parametro", "resultado_esperado"),
+    [(2019, 1), (2022, 1), (2023, 1)]
+)
+def test_se_tamanho_de_cursos_por_ano_eh_1_se_passar_nenhum_curso(parametro, resultado_esperado):
+    search_bar = Courses_list(parametro)
+    resultado = len(search_bar.search_course(""))
+    assert resultado == resultado_esperado
 
-    def test_campus(self):
-        self.setup_method()
-        result = self.search_bar.search_course("")
-        alt_result = self.search_bar.search_course("Admnistração")
-        for i in result:
-            assert i[1] in self.search_bar.CAMPUS_LIST
-        for j in result:
-            assert j[1] in self.search_bar.CAMPUS_LIST
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    ("parametro", "resultado_esperado"),
+    [(2019, 0), (2022, 0), (2023, 0)]
+)
+def test_se_tamanho_de_cursos_por_ano_eh_dif_de_0_se_passar_nenhum_curso(parametro, resultado_esperado):
+    search_bar = Courses_list(parametro)
+    resultado = len(search_bar.search_course(""))
+    assert resultado == resultado_esperado
+
+@pytest.mark.parametrize(
+    ("parametro"),
+    [2019, 2022, 2023]
+)
+def test_encontra_curso_que_se_deseja(parametro):
+    search_bar = Courses_list(parametro)
+    assert isinstance(search_bar, Courses_list)
+
+
+#    def test_with_finder(self):
+#        self.setup_method()
+#        self.finder("Adm")
+#        self.finder("")
+#        self.finder("Bacharelado")
+#        self.finder("Engenharia")
+#        self.finder("dnoawkdnsknawod")
+
+#    def test_campus(self):
+#        self.setup_method()
+#        result = self.search_bar.search_course("")
+#        alt_result = self.search_bar.search_course("Admnistração")
+#        for i in result:
+#            assert i[1] in self.search_bar.CAMPUS_LIST
+# for j in result:
+# assert j[1] in self.search_bar.CAMPUS_LIST
