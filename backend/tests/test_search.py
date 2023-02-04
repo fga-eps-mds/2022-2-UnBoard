c9f5b313a3b770a09413a20be2b0949348c27d3a
Raphides <89037051+Raphides@users.noreply.github.com>
2023-02-03 21:27:32 -0300

    Adicionando casos de teste antigos
    Necessita de atualização

---
+++ backend/tests/test_search.py
@@ -0,0 +1,34 @@
+import pytest
+from app.search_bar import *
+
+class Test_Search:
+    def setup_method(self):
+        self.search_bar = Courses_list("../../databases")
+    
+    def test_len(self):
+        self.setup_method()
+        result = self.search_bar.search_course("")
+        courses = self.search_bar.ordered_courses
+        assert (len(courses) == 101) and (len(courses) == len(result))
+
+    def finder(self, exp: str):
+        result = self.search_bar.search_course(exp)
+        for course in result:
+            assert (str(course[0]).find(exp)) != -1
+
+    def test_with_finder(self):
+        self.setup_method()
+        self.finder("Adm")
+        self.finder("")
+        self.finder("Bacharelado")
+        self.finder("Engenharia")
+        self.finder("dnoawkdnsknawod")
+
+    def test_campus(self):
+        self.setup_method()
+        result = self.search_bar.search_course("")
+        alt_result = self.search_bar.search_course("Admnistração")
+        for i in result:
+            assert i[1] in self.search_bar.CAMPUS_LIST
+        for j in result:
+            assert j[1] in self.search_bar.CAMPUS_LIST
