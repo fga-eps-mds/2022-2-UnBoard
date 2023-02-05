44b17d9098207090c2dd566cfb7b684ad2215654
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-02-05 17:47:43 -0300

    faz com que mude para o ano selecionado

--- src/Sidebars/sidebar.js
+++ src/Sidebars/sidebar.js
@@ -2,30 +2,30 @@ import React from "react";
 import "./sidebar.css"
 import { slide as Menu } from 'react-burger-menu';
 
-const SideBar = () => {
+const SideBar = (props) => {
     return(
         <Menu>
             <h3>Ano do Vestibular</h3>
             <br></br>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(23)}>
                 2023
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(22)}>
                 2022
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(19)}>
                 2019
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(18)}>
                 2018
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(17)}>
                 2017
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(16)}>
                 2016
             </a>
-            <a className="menu-item" href="/dashboards">
+            <a className="menu-item" onClick={() => props.onYearSelection(15)}>
                 2015
             </a>
     </Menu>
--- src/views/Dashboards.js
+++ src/views/Dashboards.js
@@ -3,24 +3,32 @@ import SideBar from "../Sidebars/sidebar";
 import "../App.css";
 
 class Dashboards extends React.Component {
+    handleYearSelection = (selectedYear) => {
+        this.setState({ year: selectedYear })
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+        year: 2023,
+        };
+    }
+    
+
     render() {
         return (
             <>
-                <SideBar></SideBar>
+                <SideBar onYearSelection={this.handleYearSelection}/>
 
                 <div className="App">
                     <div className="search">
                         <h4>Pesquisar curso</h4>
                         <input type="text" className="search-course" />
                     </div>
-                    <center><h1>Vestibular 2023 - UnB</h1></center><br></br><br></br>
+                    <center><h1>Vestibular 20 {this.state.year} - UnB</h1></center><br></br><br></br>
                     <div className="img-d">
                         <center><iframe src="http://localhost:8050/" width="1500" height="700"></iframe></center>
                     </div>
                 </div>
-
-
-
 
             </>
         )
