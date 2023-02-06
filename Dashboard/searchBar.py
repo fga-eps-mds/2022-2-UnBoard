d985020db311df6a2b40d3ccedb13ea4e9b83b32
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-02-05 22:48:10 -0300

    Create searchBar.py

---
+++ Dashboard/searchBar.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from buscarCurso import *
+
+if (year == 15):
+    courses = df.iloc[9:67][1].tolist()
+    print(courses)
+
+if (year == 16):
+    courses = df.iloc[18:77][1].tolist()
+    print(courses)
+
+if (year == 17):
+    courses = df.iloc[9:68][1].tolist()
+    print(courses)
+
+if (year == 18):
+    courses = df.iloc[9:68][1].tolist()
+    print(courses)
+
+if (year == 19):
+    courses = df.iloc[11:70][1].tolist()
+    print(courses)
+
+if (year == 22):
+    courses = df.iloc[12:73][1].tolist()
+    print(courses)
+
+if (year == 23):
+    courses = df.iloc[12:74][1].tolist()
+    print(courses)
